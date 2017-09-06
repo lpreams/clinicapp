3f1d9ce472384e855d2150b481f6f556cd57d14c
lpreams <lpreams@gmail.com>
2017-09-06 17:07:21 -0400

    testing, hopefully for the last time, the slack integration

--- app/src/main/java/net/carolinaphoenix/clinicappdemo/CheckInActivity.java
+++ app/src/main/java/net/carolinaphoenix/clinicappdemo/CheckInActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 /**
  * This Activity allows the user to check in a patient.
  *
- * Testing. 1 2 3 4 5 6
+ * Testing. 1 2 3 4 5 6 7 8 9 10
  */
 public class CheckInActivity extends AppCompatActivity {
 
